22f7f4c719a4ebf076e8f8be79018b394259a297
yihuiyu <yihuiyu123@163.com>
2021-10-17 22:47:50 +0800

    add d2

--- app/src/main/java/com/example/testgit/test1.kt
+++ app/src/main/java/com/example/testgit/test1.kt
@@ -1,3 +1,5 @@
 package com.example.testgit
 
-data class text(var a: String, var boolean: Boolean, var d: Double, var g: String,var h:HashMap<String,String>)
+import android.renderscript.Double2
+
+data class text(var a: String, var boolean: Boolean, var d: Double, var g: String,var h:HashMap<String,String>,var double2: Double)
